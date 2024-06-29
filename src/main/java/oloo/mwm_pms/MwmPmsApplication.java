c993067c5dcc419fe8f1c1e40400ecc7564f2f30
Olooce <asirastephen9@gmail.com>
2024-06-29 16:58:59 +0300

    Initial Commit.

---
+++ src/main/java/oloo/mwm_pms/MwmPmsApplication.java
@@ -0,0 +1,13 @@
+package oloo.mwm_pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwmPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwmPmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oloo/mwm_pms/MwmPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package oloo.mwm_pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwmPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
