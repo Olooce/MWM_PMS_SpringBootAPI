cae59d0afacfbbb061be8a5273e84b56438f00aa
Olooce <asirastephen9@gmail.com>
2024-06-29 21:01:00 +0300

    fix: resolve bugs

---
+++ src/main/java/oloo/mwm_pms/controllers/AllowanceController.java
@@ -0,0 +1,38 @@
+package oloo.mwm_pms.controllers;
+
+import oloo.mwm_pms.entities.Allowance;
+import oloo.mwm_pms.repositories.AllowanceRepository;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/allowances")
+public class AllowanceController {
+
+    private final AllowanceRepository allowanceRepository;
+
+    public AllowanceController(AllowanceRepository allowanceRepository) {
+        this.allowanceRepository = allowanceRepository;
+    }
+
+    @GetMapping
+    public PagedModel<Allowance> getAllAllowances(@RequestParam(defaultValue = "0") int page,
+                                                  @RequestParam(defaultValue = "10") int size) {
+        List<Allowance> allowances = allowanceRepository.findAll(page, size);
+        int totalAllowances = allowanceRepository.count();
+        Pageable pageable = PageRequest.of(page, size);
+        PageImpl<Allowance> allowancePage = new PageImpl<>(allowances, pageable, totalAllowances);
+
+        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(size, page, totalAllowances);
+        WebMvcLinkBuilderFactory factory = new WebMvcLinkBuilderFactory();
+        WebMvcLinkBuilder linkBuilder = factory.linkTo(WebMvcLinkBuilder.methodOn(AllowanceController.class).getAllAllowances(page, size));
+        return PagedModel.of(allowances, pageMetadata, linkBuilder.withSelfRel());
+    }
+}
