dd6efe7e6e096f7144e99b1e142b7967b0c43e10
Olooce <asirastephen9@gmail.com>
2024-06-29 20:59:00 +0300

    style: improve code formatting

---
+++ src/main/java/oloo/mwm_pms/controllers/EmployeeController.java
@@ -0,0 +1,38 @@
+package oloo.mwm_pms.controllers;
+
+import oloo.mwm_pms.entities.Employee;
+import oloo.mwm_pms.repositories.EmployeeRepository;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping
+    public PagedModel<Employee> getAllEmployees(@RequestParam(defaultValue = "0") int page,
+                                                @RequestParam(defaultValue = "10") int size) {
+        List<Employee> employees = employeeRepository.findAll(page, size);
+        int totalEmployees = employeeRepository.count();
+        Pageable pageable = PageRequest.of(page, size);
+        PageImpl<Employee> employeePage = new PageImpl<>(employees, pageable, totalEmployees);
+
+        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(size, page, totalEmployees);
+        WebMvcLinkBuilderFactory factory = new WebMvcLinkBuilderFactory();
+        WebMvcLinkBuilder linkBuilder = factory.linkTo(WebMvcLinkBuilder.methodOn(EmployeeController.class).getAllEmployees(page, size));
+        return PagedModel.of(employees, pageMetadata, linkBuilder.withSelfRel());
+    }
+}
