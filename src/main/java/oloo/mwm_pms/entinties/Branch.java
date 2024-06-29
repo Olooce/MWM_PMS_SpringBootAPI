6c4be82f31959f58446d02affd3d50e5463b7c45
Olooce <asirastephen9@gmail.com>
2024-06-29 19:17:01 +0300

    refactor: optimize code

---
+++ src/main/java/oloo/mwm_pms/entinties/Branch.java
@@ -0,0 +1,15 @@
+package com.example.jdbcdemo.model;
+
+import java.time.LocalDateTime;
+
+public class Branch {
+    private Long branchId;
+    private String branchName;
+    private String branchCode;
+    private Long noEmployees;
+    private String description;
+    private LocalDateTime dateCreated;
+    private LocalDateTime dateModified;
+
+    // Getters and Setters
+}
