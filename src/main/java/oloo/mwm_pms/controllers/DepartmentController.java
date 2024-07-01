b293aa2d0b2f2bc666eb9d56da032c40be7edc28
Olooce <asirastephen9@gmail.com>
2024-07-01 10:42:00 +0300

    chore: perform maintenance tasks

--- src/main/java/oloo/mwm_pms/controllers/DepartmentController.java
+++ src/main/java/oloo/mwm_pms/controllers/DepartmentController.java
@@ -1,38 +1,27 @@
 package oloo.mwm_pms.controllers;
 
+
 import oloo.mwm_pms.entinties.Department;
-import oloo.mwm_pms.repositories.DepartmentRepository;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import oloo.mwm_pms.services.DepartmentService;
 import org.springframework.hateoas.PagedModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/departments")
 public class DepartmentController {
+    private final DepartmentService departmentService;
 
-    private final DepartmentRepository departmentRepository;
-
-    public DepartmentController(DepartmentRepository departmentRepository) {
-        this.departmentRepository = departmentRepository;
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
     }
 
     @GetMapping
-    public PagedModel<Department> getAllDepartments(@RequestParam(defaultValue = "0") int page,
-                                                    @RequestParam(defaultValue = "10") int size) {
-        List<Department> departments = departmentRepository.findAll(page, size);
-        int totalDepartments = departmentRepository.count();
-        Pageable pageable = PageRequest.of(page, size);
-        PageImpl<Department> departmentPage = new PageImpl<>(departments, pageable, totalDepartments);
-
-        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(size, page, totalDepartments);
-        WebMvcLinkBuilderFactory factory = new WebMvcLinkBuilderFactory();
-        WebMvcLinkBuilder linkBuilder = factory.linkTo(WebMvcLinkBuilder.methodOn(DepartmentController.class).getAllDepartments(page, size));
-        return PagedModel.of(departments, pageMetadata, linkBuilder.withSelfRel());
+    public PagedModel<Department> getDepartments(@RequestParam(defaultValue = "0") int page,
+                                                 @RequestParam(defaultValue = "10") int size) {
+        return departmentService.getAllDepartments(page, size);
     }
+
+
 }
