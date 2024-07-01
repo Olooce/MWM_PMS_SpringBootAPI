5ce21eb31a3180395045962e62df35e7cb3c78de
Olooce <asirastephen9@gmail.com>
2024-07-01 11:29:01 +0300

    docs: update documentation

--- src/main/java/oloo/mwm_pms/controllers/EmployeeController.java
+++ src/main/java/oloo/mwm_pms/controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package oloo.mwm_pms.controllers;
 
 import oloo.mwm_pms.entinties.Employee;
 import oloo.mwm_pms.services.EmployeeService;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -17,8 +18,8 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
     @GetMapping
-    public List<Employee> getAllEmployee(@RequestParam(defaultValue = "0") int page,
-                                       @RequestParam(defaultValue = "10") int size) {
+    public PagedModel<Employee> getAllEmployee(@RequestParam(defaultValue = "0") int page,
+                                               @RequestParam(defaultValue = "10") int size) {
         return employeeService.getAllEmployee(page, size);
     }
 
