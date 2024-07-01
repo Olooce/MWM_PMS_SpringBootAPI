fd9a27999554385d690ab9d677b38472dba4022c
Olooce <asirastephen9@gmail.com>
2024-07-01 09:32:01 +0300

    test: add or update tests

--- src/main/java/oloo/mwm_pms/controllers/AllowanceController.java
+++ src/main/java/oloo/mwm_pms/controllers/AllowanceController.java
@@ -2,45 +2,22 @@ package oloo.mwm_pms.controllers;
 
 
 import oloo.mwm_pms.entinties.Allowance;
-
 import oloo.mwm_pms.services.AllowanceService;
-
-import org.springframework.data.domain.PageImpl;
-
-import org.springframework.data.domain.Pageable;
-
 import org.springframework.hateoas.PagedModel;
-
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
-
 @RequestMapping("/allowances")
-
 public class AllowanceController {
-
-
     private final AllowanceService allowanceService;
-
-
     public AllowanceController(AllowanceService allowanceService) {
-
         this.allowanceService = allowanceService;
-
     }
 
 
     @GetMapping
-
     public PagedModel<Allowance> getAllAllowances(@RequestParam(defaultValue = "0") int page,
-
                                                   @RequestParam(defaultValue = "10") int size) {
-
         return allowanceService.getAllAllowances(page, size);
-
     }
-
 }
