f9ab7e787783a5aa838344c78d89cb17c5f0ac27
Olooce <asirastephen9@gmail.com>
2024-07-01 09:35:00 +0300

    refactor: optimize code

--- src/main/java/oloo/mwm_pms/services/AllowanceService.java
+++ src/main/java/oloo/mwm_pms/services/AllowanceService.java
@@ -12,10 +12,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public class AllowanceService {
     private final AllowanceRepository allowanceRepository;
     public AllowanceService(AllowanceRepository allowanceRepository) {
