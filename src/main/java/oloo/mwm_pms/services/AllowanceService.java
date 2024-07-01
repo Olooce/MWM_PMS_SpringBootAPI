24a00247197209d72779805591afbe252a035d89
Olooce <asirastephen9@gmail.com>
2024-07-01 09:21:00 +0300

    chore: perform maintenance tasks

--- src/main/java/oloo/mwm_pms/services/AllowanceService.java
+++ src/main/java/oloo/mwm_pms/services/AllowanceService.java
@@ -2,19 +2,21 @@ package oloo.mwm_pms.services;
 
 import oloo.mwm_pms.controllers.AllowanceController;
 import oloo.mwm_pms.entinties.Allowance;
+import oloo.mwm_pms.repositories.AllowanceRepository;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.List;
 
+private final AllowanceRepository allowanceRepository;
+
 public class AllowanceService {
-    public PagedModel<Allowance> getAllAllowances(@RequestParam(defaultValue = "0") int page,
-                                                  @RequestParam(defaultValue = "10") int size) {
+    public static PagedModel<Allowance> getAllAllowances(int page, int size) {
         List<Allowance> allowances = allowanceRepository.findAll(page, size);
         int totalAllowances = allowanceRepository.count();
         Pageable pageable = PageRequest.of(page, size);
