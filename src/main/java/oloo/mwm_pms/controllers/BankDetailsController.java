b20865cd92195917c325e6b1d580a413fb81df17
Olooce <asirastephen9@gmail.com>
2024-07-01 10:05:00 +0300

    feat: add functionality

--- src/main/java/oloo/mwm_pms/controllers/BankDetailsController.java
+++ src/main/java/oloo/mwm_pms/controllers/BankDetailsController.java
@@ -1,39 +1,22 @@
 package oloo.mwm_pms.controllers;
 
 import oloo.mwm_pms.entinties.BankDetails;
-import oloo.mwm_pms.repositories.BankDetailsRepository;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import oloo.mwm_pms.services.BankDetailsService;
 import org.springframework.hateoas.PagedModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/bankdetails")
 public class BankDetailsController {
+    private final BankDetailsService bankDetailsService;
 
-    private final BankDetailsRepository bankDetailsRepository;
-
-    public BankDetailsController(BankDetailsRepository bankDetailsRepository) {
-        this.bankDetailsRepository = bankDetailsRepository;
+    public BankDetailsController(BankDetailsService bankDetailsService) {
+        this.bankDetailsService = bankDetailsService;
     }
 
     @GetMapping
     public PagedModel<BankDetails> getAllBankDetails(@RequestParam(defaultValue = "0") int page,
                                                      @RequestParam(defaultValue = "10") int size) {
-        List<BankDetails> bankDetailsList = bankDetailsRepository.findAll(page, size);
-        int totalBankDetails = bankDetailsRepository.count();
-        Pageable pageable = PageRequest.of(page, size);
-        PageImpl<BankDetails> bankDetailsPage = new PageImpl<>(bankDetailsList, pageable, totalBankDetails);
-
-        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(size, page, totalBankDetails);
-        WebMvcLinkBuilderFactory factory = new WebMvcLinkBuilderFactory();
-        WebMvcLinkBuilder linkBuilder = factory.linkTo(WebMvcLinkBuilder.methodOn(BankDetailsController.class).getAllBankDetails(page, size));
-        return PagedModel.of(bankDetailsList, pageMetadata, linkBuilder.withSelfRel());
+        return bankDetailsService.getAllBankDetails(page, size);
     }
-
 }
