414f94cbc866195afdc1bd13fa5b3f66692ee508
Olooce <asirastephen9@gmail.com>
2024-07-01 10:23:01 +0300

    test: add or update tests

--- src/main/java/oloo/mwm_pms/controllers/ContactInfoController.java
+++ src/main/java/oloo/mwm_pms/controllers/ContactInfoController.java
@@ -1,10 +1,23 @@
 package oloo.mwm_pms.controllers;
 
+import oloo.mwm_pms.entinties.ContactInfo;
+import oloo.mwm_pms.services.ContactInfoService;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contactinfos")
 public class ContactInfoController {
+    public final ContactInfoService contactInfoService;
+
+    public ContactInfoController(ContactInfoService contactInfoService) {
+        this.contactInfoService = contactInfoService;
+    }
+
+    public PagedModel<ContactInfo> getAllContactInfos(@RequestParam(defaultValue = "0") int page,
+                                                      @RequestParam(defaultValue = "10") int size) {
+        return contactInfoService.getAllContactInfos(page, size);
+    }
 
 
 }
