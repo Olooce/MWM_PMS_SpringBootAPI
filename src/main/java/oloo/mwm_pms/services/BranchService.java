211849ff0b81c3f71b0f7cfa2959e0e1c41f96bd
Olooce <asirastephen9@gmail.com>
2024-07-01 10:09:00 +0300

    chore: perform maintenance tasks

--- src/main/java/oloo/mwm_pms/services/BranchService.java
+++ src/main/java/oloo/mwm_pms/services/BranchService.java
@@ -1,4 +1,36 @@
 package oloo.mwm_pms.services;
 
+import oloo.mwm_pms.controllers.BranchController;
+import oloo.mwm_pms.entinties.Branch;
+import oloo.mwm_pms.repositories.BranchRepository;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BranchService {
+    private final BranchRepository branchRepository;
+
+    public BranchService(BranchRepository branchRepository) {
+        this.branchRepository = branchRepository;
+    }
+
+
+    public PagedModel<Branch> getAllBranches( int page, int size) {
+        List<Branch> branches = branchRepository.findAll(page, size);
+        int totalBranches = branchRepository.count();
+        Pageable pageable = PageRequest.of(page, size);
+        PageImpl<Branch> branchPage = new PageImpl<>(branches, pageable, totalBranches);
+
+        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(size, page, totalBranches);
+        WebMvcLinkBuilderFactory factory = new WebMvcLinkBuilderFactory();
+        WebMvcLinkBuilder linkBuilder = factory.linkTo(WebMvcLinkBuilder.methodOn(BranchController.class).getAllBranches(page, size));
+        return PagedModel.of(branches, pageMetadata, linkBuilder.withSelfRel());
+    }
 }
