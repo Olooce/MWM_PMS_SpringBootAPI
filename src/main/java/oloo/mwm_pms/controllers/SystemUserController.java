0be3a8ba0839c2dc43b2d76a24c08bdd86ee93ed
Olooce <asirastephen9@gmail.com>
2024-07-03 10:34:00 +0300

    ci: enhance CI/CD configurations

--- src/main/java/oloo/mwm_pms/controllers/SystemUserController.java
+++ src/main/java/oloo/mwm_pms/controllers/SystemUserController.java
@@ -1,17 +1,9 @@
 package oloo.mwm_pms.controllers;
 
 import oloo.mwm_pms.entinties.SystemUser;
-import oloo.mwm_pms.repositories.SystemUserRepository;
 import oloo.mwm_pms.services.SystemUserService;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilderFactory;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/systemusers")
