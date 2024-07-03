1f967b6318d9838575c5d2c7e1ca8e7dd608267b
Olooce <asirastephen9@gmail.com>
2024-07-03 14:39:00 +0300

    refactor: optimize code

--- src/main/java/oloo/mwm_pms/AppConfig.java
+++ src/main/java/oloo/mwm_pms/AppConfig.java
@@ -1,3 +1,5 @@
+package oloo.mwm_pms;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
