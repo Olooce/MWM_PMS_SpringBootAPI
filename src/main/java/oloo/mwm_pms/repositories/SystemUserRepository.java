0d5928893440b935db9262e4875b4e1031bf8311
Olooce <asirastephen9@gmail.com>
2024-07-03 14:34:01 +0300

    style: improve code formatting

--- src/main/java/oloo/mwm_pms/repositories/SystemUserRepository.java
+++ src/main/java/oloo/mwm_pms/repositories/SystemUserRepository.java
@@ -34,7 +34,7 @@ public class SystemUserRepository {
     }
 
     public SystemUser findByUsername(String username) {
-        return jdbcTemplate.queryForObject("SELECT * FROM  system_users WHERE username = ? LIMIT 1",)
+        return jdbcTemplate.queryForObject("SELECT * FROM  system_users WHERE username = ? LIMIT 1", new SystemUserRowMapper(), username);
     }
 
     private static class SystemUserRowMapper implements RowMapper<SystemUser> {
