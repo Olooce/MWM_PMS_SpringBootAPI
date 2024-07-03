267227879a6c708e7366e26306dfdb21783b019f
Olooce <asirastephen9@gmail.com>
2024-07-03 14:27:00 +0300

    style: improve code formatting

--- src/main/java/oloo/mwm_pms/controllers/SystemUserController.java
+++ src/main/java/oloo/mwm_pms/controllers/SystemUserController.java
@@ -3,6 +3,8 @@ package oloo.mwm_pms.controllers;
 import oloo.mwm_pms.entinties.SystemUser;
 import oloo.mwm_pms.services.SystemUserService;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,12 +24,17 @@ public class SystemUserController {
        return systemUserService.getAllSystemUsers(page, size);
     }
 
-    @GetMapping("/auth")
-    public autheticateUser(@RequestParam String username, @RequestParam String password){
-        //hashpassword
-        //check if usernname and password entry exist in the system_users table
-        //if existss return succesfull authentiactionstatus
-        //if not return unsecess autheticationsatauts
+    public ResponseEntity<String> authenticateUser(@RequestParam String username, @RequestParam String password) {
+        // Find the user by username
+        SystemUser user = SystemUserService.findByUsername(username);
+
+        // If user not found or password doesn't match, return unsuccessful authentication status
+        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
+            return new ResponseEntity<>( HttpStatus.UNAUTHORIZED);
+        }
+
+        // Return successful authentication status
+        return new ResponseEntity<>( HttpStatus.OK);
     }
 
 }
