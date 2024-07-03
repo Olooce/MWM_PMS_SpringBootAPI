5fd697c3f1f95790c27fef8209a1eb48a8e585df
Olooce <asirastephen9@gmail.com>
2024-07-03 10:05:01 +0300

    feat: add functionality

--- src/main/java/oloo/mwm_pms/services/SalaryService.java
+++ src/main/java/oloo/mwm_pms/services/SalaryService.java
@@ -1,10 +1,15 @@
 package oloo.mwm_pms.services;
 
+import oloo.mwm_pms.controllers.AllowanceController;
+import oloo.mwm_pms.controllers.SalaryController;
 import oloo.mwm_pms.entinties.Salary;
 import oloo.mwm_pms.repositories.SalaryRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
@@ -40,7 +45,13 @@ public class SalaryService {
 
     public PagedModel<Salary> getAllSalaries(int page,
                                              int size) {
-        return salaryRepository.findAll(page, size);
+        List<Salary>  salaries = salaryRepository.findAll(page, size);
+        int salaryCount = salaryRepository.count();
+        Pageable pageable = PageRequest.of(page, size);
+
+        PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(size, page, salaryCount);
+        WebMvcLinkBuilder linkBuilder = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(SalaryController.class).getAllSalaries(page, size));
+        return PagedModel.of(salaries, pageMetadata, linkBuilder.withSelfRel());
     }
 
     public int countSalaries() {
