6a34bd085967a0632de72f59241a0079c8459ab8
Olooce <asirastephen9@gmail.com>
2024-07-10 09:45:01 +0300

    chore: perform maintenance tasks

--- src/main/java/oloo/mwm_pms/dtos/EmployeeDTO.java
+++ src/main/java/oloo/mwm_pms/dtos/EmployeeDTO.java
@@ -16,36 +16,63 @@ public class EmployeeDTO {
     public String getName() {
         return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public LocalDate getDob() {
         return dob;
+    }
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
     }
 
     public String getGender() {
         return gender;
     }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
     public Long getDepartmentId() {
         return departmentId;
+    }
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
     }
 
     public String getEmploymentType() {
         return employmentType;
     }
+    public void setEmploymentType(String employmentType) {
+        this.employmentType = employmentType;
+    }
 
     public LocalDate getEmploymentDate() {
         return employmentDate;
+    }
+    public void setEmploymentDate(LocalDate employmentDate) {
+        this.employmentDate = employmentDate;
     }
 
     public String getStatus() {
         return status;
     }
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
     public String getStatusDescription() {
         return statusDescription;
     }
+    public void setStatusDescription(String statusDescription) {
+        this.statusDescription = statusDescription;
+    }
 
     public LocalDate getTerminationDate() {
         return terminationDate;
+    }
+    public void setTerminationDate(LocalDate terminationDate) {
+        this.terminationDate = terminationDate;
     }
 }
