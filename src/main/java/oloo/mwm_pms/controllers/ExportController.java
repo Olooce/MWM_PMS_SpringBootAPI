d5d0d24c4e0cd49afea20d35182adf30c3298bc9
Olooce <asirastephen9@gmail.com>
2024-07-21 15:13:50 +0300

    .

---
+++ src/main/java/oloo/mwm_pms/controllers/ExportController.java
@@ -0,0 +1,31 @@
+package oloo.mwm_pms.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/export")
+public class ExportController {
+
+    @Autowired
+    private ExportService exportService;
+
+    @GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public void exportToExcel(HttpServletResponse response) {
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setHeader("Content-Disposition", "attachment; filename=data.xlsx");
+
+        try {
+            exportService.exportDataToExcel(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
