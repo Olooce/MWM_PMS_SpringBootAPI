1e5ca8b7d478c601d28dbc43d05d28991531416c
Olooce <asirastephen9@gmail.com>
2024-07-22 07:50:00 +0300

    Updates

---
+++ src/main/java/oloo/mwm_pms/repositories/DataRepository.java
@@ -0,0 +1,28 @@
+package oloo.mwm_pms.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+@Repository
+public class DataRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<String> getTableHeaders(String tableName) {
+        return jdbcTemplate.query(
+                "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ?",
+                new Object[]{tableName},
+                (rs, rowNum) -> rs.getString("COLUMN_NAME")
+        );
+    }
+
+    public void getTableData(String tableName, RowCallbackHandler callbackHandler) {
+        jdbcTemplate.query("SELECT * FROM " + tableName, callbackHandler);
+    }
+}
---
+++ src/main/java/oloo/mwm_pms/services/ExportService.java
@@ -0,0 +1,50 @@
+package oloo.mwm_pms.services;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.util.List;
+
+@Service
+public class ExportService {
+
+    @Autowired
+    private DataRepository dataRepository;
+
+    public void exportDataToExcel(HttpServletResponse response) throws IOException {
+        try (Workbook workbook = new XSSFWorkbook()) {
+            List<String> headers = dataRepository.getTableHeaders("your_table");
+
+            Sheet sheet = workbook.createSheet("Data");
+            createHeaderRow(sheet, headers);
+
+            dataRepository.getTableData("your_table", rs -> {
+                int rowNum = sheet.getLastRowNum();
+                while (rs.next()) {
+                    Row row = sheet.createRow(++rowNum);
+                    for (int i = 0; i < headers.size(); i++) {
+                        Cell cell = row.createCell(i);
+                        cell.setCellValue(rs.getString(headers.get(i)));
+                    }
+                }
+            });
+
+            workbook.write(response.getOutputStream());
+            response.flushBuffer();
+        }
+    }
+
+    private void createHeaderRow(Sheet sheet, List<String> headers) {
+        Row headerRow = sheet.createRow(0);
+        for (int i = 0; i < headers.size(); i++) {
+            Cell cell = headerRow.createCell(i);
+            cell.setCellValue(headers.get(i));
+        }
+    }
+}
+
