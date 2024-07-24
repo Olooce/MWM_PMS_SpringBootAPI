04716a8c8eae102331e2b0cf49c3938ea65bd461
Olooce <asirastephen9@gmail.com>
2024-07-24 20:10:00 +0300

    Update

--- src/main/java/oloo/mwm_pms/DataSourceConfig.java
+++ src/main/java/oloo/mwm_pms/DataSourceConfig.java
@@ -3,7 +3,6 @@ package oloo.mwm_pms;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -32,9 +31,4 @@ public class DataSourceConfig {
         dataSource.setPassword(password);
         return dataSource;
     }
-
-//    @Bean
-//    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-//        return new JdbcTemplate(dataSource);
-//    }
 }
