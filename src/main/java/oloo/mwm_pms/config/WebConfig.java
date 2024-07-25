5534038c14165caff5edc2b7a6e4d8bc984a2bdb
Olooce <asirastephen9@gmail.com>
2024-07-25 09:30:01 +0300

    More updates...XD

--- src/main/java/oloo/mwm_pms/config/WebConfig.java
+++ src/main/java/oloo/mwm_pms/config/WebConfig.java
@@ -4,11 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     private final Set<String> blockedOrigins = new HashSet<>();
 
     public WebConfig() {
@@ -19,18 +22,20 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
                 .allowedHeaders("*")
-                .exposedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers");
-        
-        registry.addMapping("/**").checkOrigin(origin -> {
-            if (blockedOrigins.contains(origin)) {
-                return false;
-            }
-            return true;
-        });
+                .exposedHeaders("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                .allowCredentials(true)
+                .allowedOriginPatterns("*") // Allow all origins by default
+                .allowedOriginPatterns(getAllowedOrigins().toArray(new String[0])); // Apply allowed origins pattern
+
+    }
+
+    private Set<String> getAllowedOrigins() {
+        Set<String> allowedOrigins = new HashSet<>(List.of("*")); // Start with all origins allowed
+        for (String blockedOrigin : blockedOrigins) {
+            allowedOrigins.remove(blockedOrigin);
+        }
+        return allowedOrigins;
     }
 }
-
