629f51d572b825a45229ff48e7526f30204e9562
Olooce <asirastephen9@gmail.com>
2024-07-25 14:00:00 +0300

    Updates

--- src/main/java/oloo/mwm_pms/repositories/ExportJobRepository.java
+++ src/main/java/oloo/mwm_pms/repositories/ExportJobRepository.java
@@ -1,6 +1,5 @@
 package oloo.mwm_pms.repositories;
 
-import oloo.mwm_pms.entinties.Branch;
 import oloo.mwm_pms.entinties.ExportJob;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -12,15 +11,16 @@ import java.sql.SQLException;
 public class ExportJobRepository {
     public void save(ExportJob exportJob) {
     }
+}
+ class ExportJobRowMapper implements RowMapper<ExportJob> {
+    @Override
+    public ExportJob mapRow(ResultSet rs, int rowNum) throws SQLException {
+        ExportJob exportJob = new ExportJob();
 
-    private static class BranchRowMapper implements RowMapper<Branch> {
-        @Override
-        public ExportJob mapRow(ResultSet rs, int rowNum) throws SQLException {
-            ExportJob exportJob = new ExportJob();
+        exportJob.setFileId(rs.getString("file_id"));
 
-            exportJob.setDateCreated(rs.getTimestamp("date_created").toLocalDateTime());
-            exportJob.setDateModified(rs.getTimestamp("date_modified").toLocalDateTime());
-            return exportJob;
-        }
+        exportJob.setDateCreated(rs.getTimestamp("date_created").toLocalDateTime());
+        exportJob.setDateModified(rs.getTimestamp("date_modified").toLocalDateTime());
+        return exportJob;
     }
 }
