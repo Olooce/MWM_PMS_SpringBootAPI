c837d16094398e66b022170724017354b7c52e12
Olooce <asirastephen9@gmail.com>
2024-07-26 09:20:00 +0300

    More updates

--- src/main/java/oloo/mwm_pms/services/ExportService.java
+++ src/main/java/oloo/mwm_pms/services/ExportService.java
@@ -14,6 +14,8 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import oloo.mwm_pms.repositories.DataRepository;
 
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -56,8 +58,8 @@ public class ExportService {
         setUpTempDir();
         ExportJob exportJob = initializeExportJob(fileId);
         try (Workbook workbook = new SXSSFWorkbook(1000)) {
-            processTableData(tableName, workbook, exportJob, emitter);
-            writeWorkbookToFile(workbook, exportJob);
+            long[] totalRows = processTableData(tableName, workbook, exportJob, emitter);
+            writeWorkbookToFile(workbook, exportJob, totalRows);
             emitter.send(SseEmitter.event().name("completed").data("Export completed. File ID: " + fileId));
             emitter.complete();
         } catch (IOException | SQLException e) {
@@ -73,8 +75,8 @@ public class ExportService {
         setUpTempDir();
         ExportJob exportJob = initializeExportJob(fileId);
         try (Workbook workbook = new SXSSFWorkbook(1000)) {
-            processSearchResults(tableName, searchTerm, workbook, exportJob, emitter);
-            writeWorkbookToFile(workbook, exportJob);
+            long[] totalRows = processSearchResults(tableName, searchTerm, workbook, exportJob, emitter);
+            writeWorkbookToFile(workbook, exportJob, totalRows);
             emitter.send(SseEmitter.event().name("completed").data("Export completed. File ID: " + fileId));
             emitter.complete();
         } catch (IOException | SQLException e) {
@@ -100,21 +102,21 @@ public class ExportService {
         return exportJob;
     }
 
-    private long[] processTableData(String tableName, Workbook workbook, ExportJob exportJob) throws SQLException, IOException {
-        return processData(tableName, null, workbook, exportJob, (rs, headers, columnNameIndexMap, rowCounter, sheet, dataAvailable) -> {
+    private long[] processTableData(String tableName, Workbook workbook, ExportJob exportJob, SseEmitter emitter) throws SQLException, IOException {
+        return processData(tableName, null, workbook, exportJob,emitter, (rs, headers, columnNameIndexMap, rowCounter, sheet, dataAvailable) -> {
             createExcelRow(rs, headers, columnNameIndexMap, rowCounter, sheet);
             dataAvailable[0] = false;
         });
     }
 
-    private long[] processSearchResults(String tableName, Object searchTerm, Workbook workbook, ExportJob exportJob) throws SQLException, IOException {
-        return processData(tableName, searchTerm, workbook, exportJob, (rs, headers, columnNameIndexMap, rowCounter, sheet, dataAvailable) -> {
+    private long[] processSearchResults(String tableName, Object searchTerm, Workbook workbook, ExportJob exportJob, SseEmitter emitter) throws SQLException, IOException {
+        return processData(tableName, searchTerm, workbook, exportJob,emitter, (rs, headers, columnNameIndexMap, rowCounter, sheet, dataAvailable) -> {
             createExcelRow(rs, headers, columnNameIndexMap, rowCounter, sheet);
             dataAvailable[0] = true;
         });
     }
 
-    private long[] processData(String tableName, Object searchTerm, Workbook workbook, ExportJob exportJob, RowProcessor rowProcessor) throws SQLException {
+    private long[] processData(String tableName, Object searchTerm, Workbook workbook, ExportJob exportJob,SseEmitter emitter, RowProcessor rowProcessor) throws SQLException {
         final long[] totalRowsCreated = {0};
         final long startTime = System.currentTimeMillis();
         final long[] currentTime = {startTime};
@@ -188,6 +190,12 @@ public class ExportService {
             // Update offset and check if more data is available
             offset += CHUNK_SIZE;
             moreData = dataAvailable[0];
+
+            try {
+                emitter.send(SseEmitter.event().name("progress").data("Processed rows: " + totalRowsCreated[0]));
+            } catch (IOException e) {
+                LOGGER.log(Level.WARNING, "Failed to send progress update to client", e);
+            }
         }
 
         return totalRowsCreated;
