db9ba31eb74afa64822ea1bcdfa9ae73c1e49291
Olooce <asirastephen9@gmail.com>
2024-08-05 10:10:00 +0300

    More updates

--- src/main/java/oloo/mwm_pms/filters/JwtFilter.java
+++ src/main/java/oloo/mwm_pms/filters/JwtFilter.java
@@ -36,28 +36,31 @@ public class JwtFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         final String authorizationHeader = request.getHeader("Authorization");
-
         String username = null;
         String jwt = null;
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
             username = jwtUtil.extractUsername(jwt);
+            System.out.println("Extracted Username: " + username);
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
             if (jwtUtil.validateToken(jwt, userDetails.getUsername())) {
-
                 var usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                System.out.println("Authentication set for user: " + username);
+            } else {
+                System.out.println("Invalid JWT Token");
             }
         }
+
         chain.doFilter(request, response);
     }
+
 }
