df9b981034147a9838e99bd3a54974369b49278c
Olooce <asirastephen9@gmail.com>
2024-08-15 23:10:01 +0300

    Update

--- src/main/java/oloo/mwm_pms/config/SecurityConfig.java
+++ src/main/java/oloo/mwm_pms/config/SecurityConfig.java
@@ -16,6 +16,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -45,7 +47,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.cors(withDefaults()) // Enable CORS
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/systemusers/auth").permitAll()
                         .anyRequest().authenticated()
--- src/main/java/oloo/mwm_pms/config/WebConfig.java
+++ src/main/java/oloo/mwm_pms/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -17,3 +16,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true);
     }
 }
+
