48e551f524738628518784b00af1606d9b8e9db8
Olooce <asirastephen9@gmail.com>
2024-08-16 08:40:01 +0300

    More updates

--- src/main/java/oloo/mwm_pms/controllers/SystemUserController.java
+++ src/main/java/oloo/mwm_pms/controllers/SystemUserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/systemusers")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+//@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class SystemUserController {
 
     private final SystemUserService systemUserService;
